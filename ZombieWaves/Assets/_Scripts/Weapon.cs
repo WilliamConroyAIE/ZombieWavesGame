99160a033b053ff77f52dc681f6ecc367c2b72cb
WilliamConroyAIE <s241145@students.aie.edu.au>
2024-09-10 19:57:47 +0930

    yeah abby

---
+++ ZombieWaves/Assets/_Scripts/Weapon.cs
@@ -0,0 +1,108 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [Header("Shooting")]
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn;
+    public float bulletVelocity = 30f, bulletPrefabLifeTime = 3f, shootingDelay = 2f, spreadIntensity;
+
+    public bool isShooting, readyToShoot;
+    private bool allowReset = true;
+    public int bulletsPerBurst = 3, burstBulletsLeft;
+
+    public enum ShootingMode
+    {
+        Single,
+        Burst,
+        Auto
+    }
+    public ShootingMode currentShootingMode;
+
+    //[Header("Reloading")]
+
+    [Header("Additional")]
+    public Camera playerCamera;
+
+    private void Awake()
+    {
+       readyToShoot = true;
+       burstBulletsLeft = bulletsPerBurst; 
+    }
+    private void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (currentShootingMode == ShootingMode.Auto)
+        {
+            isShooting = Input.GetKey(KeyCode.Mouse0);
+        }
+        else if (currentShootingMode == ShootingMode.Single || currentShootingMode == ShootingMode.Burst)
+        {
+            isShooting = Input.GetKeyDown(KeyCode.Mouse0);
+        }
+
+        if (readyToShoot && isShooting)
+        {
+            burstBulletsLeft = bulletsPerBurst;
+            FireWeapon();
+        }
+    }
+
+    private void FireWeapon()
+    {
+        readyToShoot = false;
+
+        Vector3 shootingDirection = CalculateDirectionAndSpread().normalized;
+
+        GameObject bullet = Instantiate(bulletPrefab, bulletSpawn.position, Quaternion.identity);
+        bullet.transform.forward = shootingDirection;
+        bullet.GetComponent<Rigidbody>().AddForce(bulletSpawn.forward.normalized * bulletVelocity, ForceMode.Impulse);
+        StartCoroutine(DestroyBulletAfterTime(bullet, bulletPrefabLifeTime));
+
+        if (allowReset)
+        {
+            Invoke("ResetShot", shootingDelay);
+            allowReset = false;
+        }
+
+        if (currentShootingMode == ShootingMode.Burst && burstBulletsLeft > 1)
+        {
+            burstBulletsLeft--;
+            Invoke("FireWeapon", shootingDelay);
+        }
+    }
+    private Vector3 CalculateDirectionAndSpread()
+    {
+        Ray ray= playerCamera.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0f));
+        RaycastHit hit;
+
+        Vector3 targetPoint;
+        if (Physics.Raycast(ray, out hit))
+        {
+            targetPoint = hit.point;
+        }
+        else
+        {
+            targetPoint = ray.GetPoint(100);
+        }
+    }
+
+    private IEnumerator DestroyBulletAfterTime(GameObject bullet, float delay)
+    {
+        yield return new WaitForSeconds(delay);
+        Destroy(bullet);
+    }
+
+    private void ResetShot()
+    {
+        readyToShoot = true;
+        allowReset = true;
+    }
+}
